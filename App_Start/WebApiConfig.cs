f12b72a10ff23fc612442a2e74d57578784ab6ec
José Roberto Mejía Pacheco <isscjrmpacheco@gmail.com>
2020-05-18 19:52:32 -0500

    Web Api
    Web Api para consultar cubo mdx con la libreria MDX implementada

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http;
+
+namespace ApiNortwind
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            var jsonFormatter = formatters.JsonFormatter;
+            var settings = jsonFormatter.SerializerSettings;
+
+
+            jsonFormatter.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.None;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            settings.Formatting = Formatting.Indented;
+            settings.NullValueHandling = NullValueHandling.Include;
+            //Modelo como se va a devolver va a ser una notacion camel case
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.EnableCors();
+
+
+
+
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Controllers/NortwindDBController.cs
@@ -0,0 +1,52 @@
+﻿using LibMdx;
+using LibMdx.Model;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ApiNortwind.Controllers
+{
+    //Todos los otigenes, todas las cabeceras y todos los verbos de metodos(get, put, post)
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("isscjrmp/nortwind")]
+    public class NortwindDBController : ApiController
+    {
+
+
+        [HttpGet]
+        [Route("GetClients")]
+        public HttpResponseMessage GetClients()
+        {
+            QueryMdx queryMdx = new QueryMdx();
+            return Request.CreateResponse(HttpStatusCode.OK, queryMdx.GetClientsQueryMdx());
+        }
+
+        [HttpPost]
+        [Route("GetDataPie")]
+        public HttpResponseMessage GetDataPie([FromBody] FilterModel filterModel)
+        {
+            QueryMdx queryMdx = new QueryMdx();
+            return Request.CreateResponse(HttpStatusCode.OK, queryMdx.GetChartDataPieQueryMdx(filterModel.Clients, filterModel.Months, filterModel.Years));
+        }
+
+
+
+        [HttpPost]
+        [Route("GetDataBar")]
+        public HttpResponseMessage GetDataBar([FromBody] FilterModel filterModel)
+        {
+
+            QueryMdx queryMdx = new QueryMdx();
+            return Request.CreateResponse(HttpStatusCode.OK, queryMdx.GetChartDataBarQueryMdx(filterModel.Clients, filterModel.Months, filterModel.Years));
+        }
+
+        [HttpPost]
+        [Route("GetLabelsBar")]
+        public HttpResponseMessage GetLabelsBar([FromBody] FilterModel filterModel)
+        {
+            QueryMdx queryMdx = new QueryMdx();
+            return Request.CreateResponse(HttpStatusCode.OK, queryMdx.GetChartLabelsDataBarQueryMdx(filterModel.Clients, filterModel.Months, filterModel.Years));
+        }
+    }
+}
